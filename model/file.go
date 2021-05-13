3bd8939e48c2a86bea23757f7bc9f2b453e54969
Hazim Arafa <hazimarafa80@gmail.com>
2021-05-13 14:49:39 -0700

    Update file.go

--- model/file.go
+++ model/file.go
@@ -1,6 +1,11 @@
 package model
 
+import (
+	guuid "github.com/google/uuid"
+)
+
 type File struct {
-	Url	  		string	 	`gorm:"primaryKey" json:"url"`
+	Url	  		string	 	`json:"url"`
+	ID			guuid.UUID	`gorm:"primary_key" json:"id"`
 	UpdatedAt 	int64     	`gorm:"autoUpdateTime" json:"-"`
 }
